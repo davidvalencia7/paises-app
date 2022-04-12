f000499085d8389bd95563463083c5935c5c61c9
David <sergio.valencia@colimasoft.com>
2022-04-12 18:47:15 -0500

    99.- Creando los modulos y los componentes basicos

---
+++ src/app/pais/pais.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PorCapitalComponent } from './page/por-capital/por-capital.component';
+import { PorPaisComponent } from './page/por-pais/por-pais.component';
+import { PorRegionComponent } from './page/por-region/por-region.component';
+import { VerPaisComponent } from './page/ver-pais/ver-pais.component';
+
+
+
+@NgModule({
+  declarations: [
+    PorCapitalComponent,
+    PorPaisComponent,
+    PorRegionComponent,
+    VerPaisComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    PorCapitalComponent,
+    PorPaisComponent,
+    PorRegionComponent,
+    VerPaisComponent
+  ]
+})
+export class PaisModule { }
