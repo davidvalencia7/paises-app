ba81494aa2dc080a3e22b28af8ee1d199fcf524f
David <sergio.valencia@colimasoft.com>
2022-04-15 12:33:27 -0500

    108.- Tipado de la peticion RestCountry

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ICountry } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,8 @@ export class PaisService {
 
   constructor(private _http : HttpClient) { }
 
-  buscarPais (termino : string) : Observable<any> {
+  buscarPais (termino : string) : Observable<ICountry[]> {
     const url = `${this.apiURL}name/${termino}`;
-    return this._http.get(url)
+    return this._http.get<ICountry[]>(url)
   }
 }
